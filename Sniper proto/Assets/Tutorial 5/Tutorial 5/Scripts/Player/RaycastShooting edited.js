49cd66e74d910276aca77c8b3a04f3f08019c6fc
Slurps <multanen.mika@gmail.com>
2015-11-17 17:57:32 +0200

    Snipupelin proto

---
+++ Sniper proto/Assets/Tutorial 5/Tutorial 5/Scripts/Player/RaycastShooting edited.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var Effect : Transform;
+var TheDammage = 100;
+var Cam : Camera;
+
+function Update () {
+	
+    var hit : RaycastHit;
+    var ray : Ray = Cam.ScreenPointToRay(Vector3(Screen.width*0.5, Screen.height*0.5, 0));
+	
+    if (Input.GetMouseButtonDown(0))
+    {
+        if (Physics.Raycast (ray, hit, 100))
+        {
+            var particleClone = Instantiate(Effect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(particleClone.gameObject, 2);
+            hit.transform.SendMessage("ApplyDammage", TheDammage, SendMessageOptions.DontRequireReceiver);
+        }
+    }
+	
+}
---
+++ Sniper proto/Assets/Tutorial 5/Tutorial 5/Scripts/Player/ShowCursor.js
@@ -0,0 +1,8 @@
+﻿//Drag Onto Main Camera
+function Start ()
+    {
+ if (Input.GetKeyDown("escape")) Cursor.visible = true;
+ if (Input.GetKeyDown("escape")) Screen.lockCursor = false;
+  
+      }
+       
